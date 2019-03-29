ebfcf82f57908d0986a3d47796229ed6e7953da5
nooldey <nooldey@gmail.com>
2019-03-29 14:50:30 +0800

    FIX: LOG SRC

--- modules/log/index.js
+++ modules/log/index.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const PATH = require('path')
 
 module.exports = function(req, res, next){
-  fs.readFile(PATH.resolve(__dirname + '../../log/req.log'), function (err, data) {
+  fs.readFile(PATH.resolve(__dirname + '/log/req.log'), function (err, data) {
     if (err) {
       res.writeHead(500);
       res.end(JSON.stringify(err));
